f9491e50ce5a3e74b60630005184d2ba2946063f
hwang <leonid100010@163.com>
2023-03-31 14:22:22 +0800

    hotspot init

---
+++ src/test/java/com/nju/edu/cn/HotSpotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nju.edu.cn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotSpotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
